ac0f3742d1108f322aa295d974518195e501328e
Lisa <lisa.hl.chen@gmail.com>
2022-04-28 14:07:01 -0700

    render template practice

---
+++ Notes.md
@@ -0,0 +1,19 @@
+# Notes
+
+https://localhost:5000/
+
+###### Must install the following:
+
+pipenv install flask==2.0.3
+
+###### Must run the following to properly work this file
+
+pip install pipenv
+
+pipenv shell
+
+###### To leave your environment:
+
+exit
+
+Pipfile will display the packages installed, and Pipfile.lock will have the specific details on what version is being used.
---
+++ server.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+# localhost:5000/
+@app.route('/')
+def home():
+    return render_template("index.html", phrase="hello", times=5)
+
+# Level 1
+# When a user visits http://localhost:5000/play, have it render three beautiful looking blue boxes. Please use a template to render this.
+@app.route('/play')
+def level_1():
+    return render_template("level_1.html")
+
+# LEVEL 2
+# When a user visits localhost:5000/play/(x), have it display the beautiful looking blue boxes x times. For example, localhost:5000/play/7 should display these blue boxes 7 times. Calling localhost:5000/play/35 would display these blue boxes 35 times. Please remember that x originally is a string, and if you want to use it as an integer, you must first convert it to integer using int(). For example int("7") returns 7.
+
+@app.route('/play/<int:num>')
+def level_2(num):
+    return render_template("level_2.html", num=num)
+
+# LEVEL 3
+# When a user visits localhost:5000/play/(x)/(color), have it display beautiful looking boxes x times, but this time where the boxes appear in (color). For example, localhost:5000/play/5/green would display 5 beautiful green boxes. Calling localhost:5000/play/35/red would display 35 beautiful red boxes.
+
+@app.route('/play/<int:num>/<string:color>')
+def level_3(num, color):
+    return render_template("level_3.html", num=num, color=color)
+
+
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run the app in debug mode.
